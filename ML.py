24b86d0375463da93077d70d48fc1becf02c8c20
Nicholas Tan Jian Zhi <58356997+nicktan96@users.noreply.github.com>
2020-10-15 17:05:51 +1100

    Add files via upload

---
+++ ML.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+
+dataset = pd.read_csv('heart10.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size =0.20)
+
+sc = preprocessing.StandardScaler()
+X_train = pd.DataFrame(sc.fit_transform(X_train))
+X_test = pd.DataFrame(sc.transform(X_test))
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 250, criterion = 'gini', max_features = 'log2', random_state=0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+pickle.dump(classifier, open('genModel.pkl', 'wb'))
---
+++ predictApp.py
@@ -0,0 +1,91 @@
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import numpy as np
+
+
+app = Flask(__name__)
+
+model=pickle.load(open('genModel.pkl','rb'))
+
+@app.route('/')
+def hello_world():
+    return render_template("heart_prediction.html")
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    # For age
+    data1 = request.form['age']
+    if data1 == "":
+        data1 = 40
+    else:
+        data1 = request.form['age']
+    # For gender
+    data2 = request.form['gender']
+    if data2 =="m" or data2 == "M":
+        data2 = 0
+    else:
+        data2 = 1
+        print(data2)
+    # For chest pain
+    data3 = request.form['chestpain']
+    if data3 == "":
+        data3 = 0
+    else:
+        data3 = request.form['chestpain']
+
+    # For Resting Blood pressure
+    data4 = request.form['bps']
+    if data4 == "":
+        data4 = 100
+    else:
+        data4 = request.form['bps']
+
+    # For Cholesterol
+    data5 = request.form['chol']
+    if data5 == "":
+        data5 = 200
+    else:
+        data5 = request.form['chol']
+
+    # For Fasting Blood pressure
+    data6 = request.form['fbp']
+    if data6 == "":
+        data6 = 0
+    else:
+        data6 = request.form['fbp']
+
+    # For Resting ECG
+    data7 = request.form['ecg']
+    if data7 == "":
+        data7 = 0
+    else:
+        data7 = request.form['ecg']
+
+    # For Maximum heart rate
+    data8 = request.form['max']
+    if data8 == "":
+        data8 = 170
+    else:
+        data8 = request.form['max']
+
+    # For exercise induced angina
+    data9 = request.form['ang']
+    if data9 == "":
+        data9 = 0
+    else:
+        data9 = request.form['ang']
+
+    arr = np.array([[data1, data2, data3, data4, data5, data6, data7, data8, data9]])
+    prediction = model.predict(arr)
+
+    output = prediction
+
+    if output == 1:
+        return render_template('highResults.html', pred='High Risk of Heart Disease'.format(output))
+
+    else:
+        return render_template('lowResults.html', pred='Low Risk of Heart Disease '.format(output))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
